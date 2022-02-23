f530ff51931c027d20facd58b0418c3c92e5aaff
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-02-22 19:27:54 -0500

    higher-res shoreline

--- windturbs/plot_turbines.py
+++ windturbs/plot_turbines.py
@@ -2,7 +2,7 @@
 
 """
 Author: Lori Garzio on 2/17/2022
-Last modified: 2/17/2022
+Last modified: 2/22/2022
 Plot the Atlantic City WEA and experimental wind turbine locations at every other grid point
 """
 
@@ -21,7 +21,7 @@ def main(f, save_file):
     # set up the map
     lccproj = ccrs.LambertConformal(central_longitude=-74.5, central_latitude=38.8)
     fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(projection=lccproj))
-    pf.add_map_features(ax, extent)
+    pf.add_map_features(ax, extent, landcolor='tan', zoom_shore=True)
 
     la_polygon, pa_polygon = cf.extract_lease_area_outlines()
     pf.add_lease_area_polygon_single(ax, la_polygon[1], '#737373')  # lease area by Atlantic City
@@ -35,6 +35,6 @@ def main(f, save_file):
 
 
 if __name__ == '__main__':
-    file = '/Users/garzio/Documents/rucool/bpu/wrf/windturbs/plots/turbine_locations_draft.csv'
+    file = '/Users/garzio/Documents/rucool/bpu/wrf/windturbs/plots/turbine_locations_final.csv'
     savefile = '/Users/garzio/Documents/rucool/bpu/wrf/windturbs/plots/wrf_1km_turbine_locations_final.png'
     main(file, savefile)
